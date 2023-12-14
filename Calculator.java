f2a17925f8e9f7781242cd23381f758ccb05e61c
[sophiann32] <[sophiann32@gmail.com]>
2023-12-14 16:22:45 +0900

    ..

--- Calculator.java
+++ Calculator.java
@@ -4,3 +4,10 @@
 			return number1+number2;
 		}
 
+
+                  int my_add(int number1, int number2){
+                        if (number1 + number2 < 0)
+                                return;
+                        return number1+number2;
+
+
