32d8b2071a2b8d773a03bbeb04ae5cc3acf2a34b
JUNGSEUNGKIM <perghost5284@gmail.com>
2023-12-14 16:39:41 +0900

    ....

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@
                                 return;
                         return number1+number2;
 }
-
+//1555555555555
 
                   int my_add(int number1, int number2){
                         if (number1 + number2 < 0)
